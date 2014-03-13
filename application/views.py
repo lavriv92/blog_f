6cc24c80c4778489fcff077f779be9a3130491a6
ilavriv <lavriv92@gmail.com>
2014-03-14 00:53:13 +0200

    Added WTF support

--- application/views.py
+++ application/views.py
@@ -1,7 +1,10 @@
 from flask import render_template
-from application import app
+from application import app, db
+from models import Category, Post
 
 
 @app.route('/')
 def index():
+    categories = Category.query.all()
+    print categories
     return render_template('core/index.html')
