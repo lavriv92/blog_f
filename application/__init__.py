e9cbc22e7bf1aa572b8772ca575e48834d265b52
ilavriv <lavriv92@gmail.com>
2014-03-15 18:21:25 +0200

    Added post templates

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'mysql://root:password@localhost/blog'
 db = SQLAlchemy(app)
 from application import views
--- application/views.py
+++ application/views.py
@@ -1,10 +1,30 @@
 from flask import render_template
-from application import app, db
+from application import app
 from models import Category, Post
 
 
 @app.route('/')
 def index():
     categories = Category.query.all()
-    print categories
-    return render_template('core/index.html')
+    context = {
+        'categories': categories
+    }
+    return render_template('core/index.html', **context)
+
+
+@app.route('/categories')
+def categories():
+    categories = Category.query.all()
+    context = {
+        'categories': categories
+    }
+    return render_template('categories.html', **context)
+
+
+@app.route('/posts')
+def posts():
+    posts = Post.query.all().limit(5)
+    context = {
+        'posts': posts
+    }
+    return render_template('posts/posts.html', **context)
